e2b1fae43879a17f15cfb4813b45f756e4a14d3d
Alice <alicia.s.alice@gmail.com>
2023-04-06 19:24:46 -0300

    Actualizado endpoint de pages

--- AppBlog/urls.py
+++ AppBlog/urls.py
@@ -5,7 +5,7 @@ from AppBlog.views import *
 urlpatterns = [
     path('pages', home, name="Home"),
     path('posts', posts, name="Posts"),
-    path('post/<int:post_id>', post, name="Posteo"),
+    path('pages/<int:post_id>', post, name="Posteo"),
     path('borrarpost/<int:post_id>', borrar_post, name="BorrarPost"),
     path('comentarpost/<int:post_id>', comentar_post, name="ComentarPost"),
     path('borrarcomentario/<int:comentario_id>', borrar_comentario, name="BorrarComentario"),
